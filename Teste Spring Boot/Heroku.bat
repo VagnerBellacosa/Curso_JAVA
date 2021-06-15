59bea90d029c488243aebca862d4c093ec7a1786
Vagner Bellacosa <vagnerbellacosa@Gmail.com>
2021-06-15 20:02:01 -0300

    Spring Boot

---
+++ Teste Spring Boot/Heroku.bat
@@ -0,0 +1,11 @@
+$ cd /pasta/do/projeto/gestao-contatos
+
+$ git init; git add .; git commit -m "Versão concluída."
+
+$ heroku login # login pelo terminal (utilize mesmo usuário e senha do cadastro)
+
+$ heroku create # cria a sua aplicação lá no Heroku
+
+$ git push heroku master # envia sua aplicação para o Heroku
+
+$ heroku open # abre sua aplicação no navegador
