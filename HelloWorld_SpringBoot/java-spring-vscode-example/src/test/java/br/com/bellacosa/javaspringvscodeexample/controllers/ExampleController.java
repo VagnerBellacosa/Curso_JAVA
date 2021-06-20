b96b4f56463198b8597323827d4e24dd00e1998a
Vagner Bellacosa <vagnerbellacosa@Gmail.com>
2021-06-20 01:05:37 -0300

    teste spring boot

---
+++ HelloWorld_SpringBoot/java-spring-vscode-example/src/test/java/br/com/bellacosa/javaspringvscodeexample/controllers/ExampleController.java
@@ -0,0 +1,21 @@
+package br.com.bellacosa.javaspringvscodeexample.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ExampleController
+ * 
+ * @author Vagner Bellacosa
+ */
+@RestController
+@RequestMapping("api/example")
+public class ExampleController {
+
+	@GetMapping("/hello-world")
+	public ResponseEntity<String> get() {
+		return ResponseEntity.ok("Hello World!");
+	}
+}
